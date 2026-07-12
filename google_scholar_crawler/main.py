794a8fbef428c682ed4c9236b522b64fb819aacd
yuchaozhi <yuchaozhi1123@gmail.com>
2026-07-12 14:42:27 +0800

    update citation

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -12,7 +12,7 @@ author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
 print(json.dumps(author, indent=2))
 os.makedirs('results', exist_ok=True)
-with open(f'results/gs_data.json', 'w') as outfile:
+with open(f'results/gs_data.json', 'w', encoding='utf-8') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
 
 shieldio_data = {
@@ -21,5 +21,5 @@ shieldio_data = {
   "message": f"{author['citedby']}",
 }
 
-with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+with open(f'results/gs_data_shieldsio.json', 'w', encoding='utf-8') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
